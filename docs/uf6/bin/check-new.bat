65fe8f137fb83d9904952e0e3950f2c72af98dcc
Richard <legalize@xmission.com>
2026-05-19 17:44:13 -0600

    Improve script to check new text files

--- docs/uf6/bin/check-new.bat
+++ docs/uf6/bin/check-new.bat
@@ -1,2 +1,2 @@
-git status *.txt | sed -e "1,/git add /d" | tr "\t" "," | sed -e "/^[^,]/d" -e "s/^,//" -e "s,/,\\,g" > c:\tmp\files.txt
+git status *.txt | sed -e "1,/git add /d" | tr "\t" "," | sed -e "/^[^,]/d" -e "s/^,//" | tr / "\\\\" > c:\tmp\files.txt
 for /f %%f in (c:\tmp\files.txt) do @(cls && ((echo %%f && type %%f ) | more) && pause)
